021ed614f7c156825993a513cfa5e6319efc8bfd
patrick_odermatt1 <patrick_odermatt1>
2016-05-12 16:13:43 +0200

    stats implemented

--- flashcards/flashcards/frmStats.Designer.cs
+++ flashcards/flashcards/frmStats.Designer.cs
@@ -28,36 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnBack = new System.Windows.Forms.Button();
-            this.progressBar1 = new flashcards.VerticalProgressBar();
             this.SuspendLayout();
-            // 
-            // btnBack
-            // 
-            this.btnBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnBack.Location = new System.Drawing.Point(12, 305);
-            this.btnBack.Name = "btnBack";
-            this.btnBack.Size = new System.Drawing.Size(713, 34);
-            this.btnBack.TabIndex = 2;
-            this.btnBack.Text = "Back";
-            this.btnBack.UseVisualStyleBackColor = true;
-            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
-            // 
-            // progressBar1
-            // 
-            this.progressBar1.Location = new System.Drawing.Point(412, 25);
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(42, 118);
-            this.progressBar1.TabIndex = 3;
-            this.progressBar1.Value = 80;
             // 
             // frmStats
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(737, 354);
-            this.Controls.Add(this.progressBar1);
-            this.Controls.Add(this.btnBack);
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.MaximumSize = new System.Drawing.Size(800, 1500);
+            this.MinimumSize = new System.Drawing.Size(800, 0);
             this.Name = "frmStats";
             this.Text = "Flashcards Administration";
             this.ResumeLayout(false);
@@ -66,8 +46,6 @@
 
         #endregion
 
-        private System.Windows.Forms.Button btnBack;
-        private VerticalProgressBar progressBar1;
 
 
 
--- flashcards/flashcards/frmStats.cs
+++ flashcards/flashcards/frmStats.cs
@@ -17,6 +17,77 @@ namespace flashcards
         {
             InitializeComponent();
             this.username = user;
+            this.Text = "Stats for user " + username;
+            this.ShowStats();
+            this.Controls[1].Select();
+        }
+
+        private void ShowStats()
+        {
+            using (var context = new Lernkartei_Entities())
+            {
+                Label lblName = new Label();
+                lblName.Text = "learning progress for user " + username;
+                lblName.AutoSize = true;
+                lblName.Font = new Font("Arial", 25);
+                lblName.Location = new System.Drawing.Point(97, 30);
+                this.Controls.Add(lblName);
+
+                int themeNameOffset = 100;
+
+                foreach(TbTheme theme in context.TbTheme)
+                {
+                    int totalCards = (from p in context.TbProgress
+                        join l in context.TbLogin on p.fk_UserID equals l.UserID
+                        join c in context.TbCard on p.fk_CardID equals c.CardID
+                        join t in context.TbTheme on c.fk_ThemeID equals t.ThemeID
+                        where l.Username == this.username
+                        where t.ThemeName == theme.ThemeName
+                        select c).Count();
+
+                    Label lblTheme = new Label();
+                    lblTheme.Text = theme.ThemeName;
+                    lblTheme.Location = new System.Drawing.Point(100, themeNameOffset);
+                    themeNameOffset = themeNameOffset + 200;                    
+                    this.Controls.Add(lblTheme);
+
+                    int cardOffset = 100;
+
+                    for(int i = 0; i < 8; i++)
+                    {
+                        List<TbCard> cards = (from p in context.TbProgress
+                            join l in context.TbLogin on p.fk_UserID equals l.UserID
+                            join c in context.TbCard on p.fk_CardID equals c.CardID
+                            join t in context.TbTheme on c.fk_ThemeID equals t.ThemeID
+                            where l.Username == this.username
+                            where t.ThemeName == theme.ThemeName
+                            where p.Level == i
+                            select c).ToList();
+
+                        VerticalProgressBar verticalProgressBar = new VerticalProgressBar();
+                        verticalProgressBar.Value = (100 / totalCards) * cards.Count();
+                        verticalProgressBar.Width = 30;
+                        verticalProgressBar.Height = 100;
+                        verticalProgressBar.Location = new System.Drawing.Point(cardOffset, themeNameOffset - 170);
+                        this.Controls.Add(verticalProgressBar);
+
+                        Label lblLevel = new Label();
+                        lblLevel.Text = Convert.ToString(i);
+                        lblLevel.AutoSize = true;
+                        lblLevel.Location = new System.Drawing.Point(cardOffset + 6, themeNameOffset - 65);
+                        this.Controls.Add(lblLevel);
+                        cardOffset = cardOffset + 75;
+                    }
+                }
+
+                Button button = new Button();
+                button.Text = "Back";
+                button.Location = new System.Drawing.Point(100, themeNameOffset);
+                button.Width = 555;
+                button.Height = 50;
+                button.Click += this.btnBack_Click;
+                this.Controls.Add(button);
+            }
         }
 
         private void btnBack_Click(object sender, EventArgs e)
