4e4c132db93e099c30360e9832fe24c20f53fcb9
patrick_odermatt1 <patrick_odermatt1>
2016-05-12 09:33:29 +0200

    Learn Cards

--- flashcards/flashcards/frmLearnCards.cs
+++ flashcards/flashcards/frmLearnCards.cs
@@ -13,12 +13,49 @@ namespace flashcards
     public partial class frmLearnCards : Form
     {
         private string themeName;
+        private List<TbCard> cardList;
+        private string username;
 
-        public frmLearnCards(string themeName)
+        public frmLearnCards(string themeName, string username)
         {
             InitializeComponent();
+            this.cardList = new List<TbCard>();
             this.themeName = themeName;
+            this.username = username;
+            this.SetFields();
+        }
+
+        private void SetFields()
+        {
             this.Text = this.themeName;
+
+            using (var context = new Lernkartei_Entities())
+            {
+                foreach(TbCard card in context.TbCard)
+                {
+                    cardList.Add(card);
+                }
+
+                long minLvls = (from p in context.TbProgress
+                    join l in context.TbLogin on p.fk_UserID equals l.UserID
+                    join c in context.TbCard on p.fk_CardID equals c.CardID
+                    join t in context.TbTheme on c.fk_ThemeID equals t.ThemeID
+                    where l.Username == this.username
+                    where t.ThemeName == this.themeName
+                    orderby p.Level ascending
+                    select p.Level).First();
+
+
+                //var results = 
+                    
+                //    from p in context.TbProgress
+                //    join l in context.TbLogin on p.fk_UserID equals l.UserID
+                //    join c in context.TbCard on p.fk_CardID equals c.CardID
+                //    where l.Username == this.username
+                //    where p.Level 
+
+
+            }
         }
 
         private void txtAnswer_KeyPress(object sender, KeyPressEventArgs e)
